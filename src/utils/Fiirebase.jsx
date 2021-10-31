08cc733f11b9f544c94aeba9e1d66ced2ab5d5ae
hrithik73 <hrithiksinghvps_cse18@its.edu.in>
2021-11-01 00:05:17 +0530

    Daily Push

--- src/utils/Fiirebase.jsx
+++ src/utils/Fiirebase.jsx
@@ -1,13 +1,14 @@
 import firebase from "firebase"
+import Constants from "expo-constants"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6KzSb-gY4IzpvnaesiAvXT0IGZlbVwYU",
-  authDomain: "coolguides-28d25.firebaseapp.com",
-  projectId: "coolguides-28d25",
-  storageBucket: "coolguides-28d25.appspot.com",
-  messagingSenderId: "197713913538",
-  appId: "1:197713913538:web:8ae3f07e45950cf56a3d5b",
-  measurementId: "G-0LXSRY5CFS",
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+  measurementId: Constants.manifest.extra.measurementId,
 }
 
 export const app = firebase.initializeApp(firebaseConfig)
